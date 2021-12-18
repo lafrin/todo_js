68a8259d8428a74154c1f88e7644dc48b5e29100
hayashida <riris.lafih.lovecat@gmail.com>
2021-12-18 17:56:37 +0900

    update:基本的なCRUD機能の実装

--- js/main.js
+++ js/main.js
@@ -1,34 +1,66 @@
 const addTaskBtn = document.getElementById('add_task')
 const addValue = document.getElementById('add_value')
-const taskList = document.getElementById('task_list')
+const addPriority = document.getElementById('add_priority')
 
-const addTask = (task) => {
+const taskList_h = document.getElementById('priority_high')
+const taskList_m = document.getElementById('priority_midole')
+const taskList_l = document.getElementById('priority_low')
+
+const priolityList = {'high': '高', 'middle': '中', 'low': '低'}
+
+const addTask = (task, priority) => {
   //valueのliを作ってリストに追加
-  const newItem = document.createElement('li')
-  const showItem = taskList.appendChild(newItem)
-  showItem.innerHTML = task;
+  const listElement = document.createElement('li')
+  if(priority == 'high'){
+    taskList_h.appendChild(listElement)
+  }else if(priority == 'middle'){
+    taskList_m.appendChild(listElement)
+  }else{
+    taskList_l.appendChild(listElement)
+  }
+  
 
-  //liに削除ボタンを追加
-  const deleteBtn = document.createElement('button')
-  deleteBtn.innerHTML = '削除'
-  newItem.appendChild(deleteBtn)
+  //内容
+  // const editButton = createAndAddListInnerElement(listElement, 'i', ['bi','bi-pencil','flex-edit'], '')
+  createAndAddListInnerElement(listElement, 'div', ['flex-value'], task)
+  const priorityJp = priolityList[priority]
+  createAndAddListInnerElement(listElement, 'div', ['flex-priority'], priorityJp)
+  const deleteButton = createAndAddListInnerElement(listElement, 'i', ['bi','bi-calendar-x','flex-delete'], '')
 
-  deleteBtn.addEventListener('click', evt => {
+  deleteButton.addEventListener('click', evt => {
     evt.preventDefault()
-    deleteTask(deleteBtn)
+    deleteTask(deleteButton)
   })
 }
 
-const deleteTask = (deleteBtn) => {
-  const chosenTask = deleteBtn.closest('li')
+//子要素を作成してli要素に追加する
+const createAndAddListInnerElement = (li, ele, classNames, value) => {
+  const element = document.createElement(ele)
+  classNames.forEach(name => {
+    element.classList.add(name)
+  });
+  element.innerHTML = value
+  li.appendChild(element);
+  return element
+}
+
+
+//タスクの削除
+const deleteTask = (deleteButton) => {
+  if( !confirm('削除しますか？') ) return
+  const chosenTask = deleteButton.closest('li')
   taskList.removeChild(chosenTask)
 }
 
 addTaskBtn.addEventListener('click', evt => {
   evt.preventDefault()
   const task = addValue.value
-  if(task.trim() == '') return
-  addTask(task)
-  console.log(task)
+  const priority = addPriority.value
+  if(task.trim() == ''){
+    alert('内容が空です')
+    return
+  }
+  addTask(task, priority)
   addValue.value = ''
+  addPriority.value = 'low'
 })
